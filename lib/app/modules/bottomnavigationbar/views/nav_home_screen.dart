d7b7238da541aaf0fe6b26fde1ad9c9f48646659
DESKTOP-96P2OAF\Adnan <adnan@bdtask.net>
2023-04-12 10:19:33 +0600

    renderFlux solve

--- lib/app/modules/bottomnavigationbar/views/nav_home_screen.dart
+++ lib/app/modules/bottomnavigationbar/views/nav_home_screen.dart
@@ -45,19 +45,9 @@ class NavHomeScreen extends StatelessWidget {
             borderRadius: BorderRadius.circular(8),
           ),
           width: MediaQuery.of(context).size.width,
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Center(
-                    child: Text(
-                  ' Product List ',
-                  style: TextStyle(fontSize: 18),
-                )),
-              ),
-              ProductListWidget(productList: controller.homeAllProductList)
-            ],
-          ),
+          child:
+
+          ProductListWidget(productList: controller.homeAllProductList)
         );
       }
     });
@@ -72,120 +62,118 @@ class ProductListWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     var productDetails = Get.put(ProductDetailsController());
     var bottomNavCTR = Get.put(BottomNavigationbarController());
-    return SingleChildScrollView(
-        child: Container(
+    return Container(
       color: Colors.grey.shade200,
       child: StaggeredGridView.countBuilder(
-        physics: NeverScrollableScrollPhysics(),
-        scrollDirection: Axis.vertical,
-        shrinkWrap: true,
-        crossAxisCount: 6,
-        itemCount: productList.length,
-        itemBuilder: (context, index) {
-          ProductModel pInfo = productList[index];
-          return InkWell(
-            onTap: () async {
-              print('product details ');
-              productDetails.productId.value = pInfo.id.toString();
-              productDetails.productInfo.value = productList[index];
-              print(productDetails.productInfo.value);
-              Get.toNamed(Routes.PRODUCT_DETAILS);
-            },
-            child: Container(
-              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(1.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: Container(
-                        height: 130,
-                        width: MediaQuery.of(context).size.width,
-                        //color: Colors.blue,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(8.0),
-                          child: Image.network(
-                            '${pInfo.image}',
-                            fit: BoxFit.fill,
-                          ),
-                        ),
+    scrollDirection: Axis.vertical,
+    shrinkWrap: true,
+    crossAxisCount: 6,
+    itemCount: productList.length,
+    itemBuilder: (context, index) {
+      ProductModel pInfo = productList[index];
+      return InkWell(
+        onTap: () async {
+          print('product details ');
+          productDetails.productId.value = pInfo.id.toString();
+          productDetails.productInfo.value = productList[index];
+          print(productDetails.productInfo.value);
+          Get.toNamed(Routes.PRODUCT_DETAILS);
+        },
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(1.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Container(
+                    height: 130,
+                    width: MediaQuery.of(context).size.width,
+                    //color: Colors.blue,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.network(
+                        '${pInfo.image}',
+                        fit: BoxFit.fill,
                       ),
                     ),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8),
+                        child: Text(
+                          pInfo.name.toString(),
+                          maxLines: 2,
+                          overflow: TextOverflow.visible,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left: 8),
-                            child: Text(
-                              pInfo.name.toString(),
-                              maxLines: 2,
-                              overflow: TextOverflow.visible,
+                          Expanded(
+                            flex: 3,
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 8),
+                              child: Text(
+                                'Price :  ৳ ${pInfo.productPrice!.price}',
+                                style: const TextStyle(
+                                  color: Color(0xFFD81D4C),
+                                  fontWeight: FontWeight.w500,
+                                  height: 1.42,
+                                ),
+                              ),
                             ),
                           ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Expanded(
-                                flex: 3,
-                                child: Padding(
-                                  padding: const EdgeInsets.only(left: 8),
-                                  child: Text(
-                                    'Price :  ৳ ${pInfo.productPrice!.price}',
-                                    style: const TextStyle(
-                                      color: Color(0xFFD81D4C),
-                                      fontWeight: FontWeight.w500,
-                                      height: 1.42,
-                                    ),
-                                  ),
+                          Expanded(
+                            child: InkWell(
+                              onTap: () {
+                                print('pId :${pInfo.id.toString()}');
+                                bottomNavCTR
+                                    .productDeleteById(pInfo.id.toString());
+                              },
+                              child: Container(
+                                height: 30,
+                                width: 30,
+                                margin: const EdgeInsets.symmetric(
+                                    vertical: 2, horizontal: 5),
+                                decoration: BoxDecoration(
+                                    color: Colors.grey.shade500,
+                                    borderRadius: BorderRadius.circular(5)),
+                                child: const Icon(
+                                  Icons.delete_forever,
+                                  size: 18,
+                                  color: Colors.white,
                                 ),
                               ),
-                              Expanded(
-                                child: InkWell(
-                                  onTap: () {
-                                    print('pId :${pInfo.id.toString()}');
-                                    bottomNavCTR
-                                        .productDeleteById(pInfo.id.toString());
-                                  },
-                                  child: Container(
-                                    height: 30,
-                                    width: 30,
-                                    margin: const EdgeInsets.symmetric(
-                                        vertical: 2, horizontal: 5),
-                                    decoration: BoxDecoration(
-                                        color: Colors.grey.shade500,
-                                        borderRadius: BorderRadius.circular(5)),
-                                    child: const Icon(
-                                      Icons.delete_forever,
-                                      size: 18,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
+                            ),
                           ),
                         ],
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
+              ],
             ),
-          );
-        },
-        staggeredTileBuilder: (int index) =>
-            StaggeredTile.count(3, index.isOdd ? 3.9 : 3.6),
-        crossAxisSpacing: 1.0,
-        mainAxisSpacing: 2.0,
+          ),
+        ),
+      );
+    },
+    staggeredTileBuilder: (int index) =>
+        StaggeredTile.count(3, index.isOdd ? 3.9 : 3.6),
+    crossAxisSpacing: 1.0,
+    mainAxisSpacing: 2.0,
       ),
-    ));
+    );
   }
 
 
